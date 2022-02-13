236cbaa519eb1afb428487dad6263a5b88db6731
sagarsheth2526 <99607041+sagarsheth2526@users.noreply.github.com>
2022-02-13 23:36:14 +0530

    programs

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace login
+{
+   class Class1
+    {
+       public SqlConnection cn = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=E:\kinjal\C#\login\login\bin\Debug\login.mdf;Integrated Security=True;User Instance=True");
+    }
+}
